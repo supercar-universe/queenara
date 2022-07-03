52d22bff2fe6764a9763fdea272b21fd7cf3daad
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2022-07-03 15:13:18 +0700

    ádsadsa

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,44 +1,46 @@
 const $ = document.querySelector.bind(document);
 let header = $("#header"),
-  fixed = header.offsetTop;
+    fixed = header.offsetTop;
 (window.onscroll = () => {
-  window.pageYOffset > fixed
-    ? header.classList.add("fixed")
-    : header.classList.remove("fixed");
+    window.pageYOffset > fixed
+        ? header.classList.add("fixed")
+        : header.classList.remove("fixed");
 }),
-  ($(".mobile_bar").onclick = () => {
-    $(".nav").classList.toggle("mobile_show");
-  });
+    ($(".mobile_bar").onclick = () => {
+        $(".nav").classList.toggle("mobile_show");
+    });
 $(".scroll_top").onclick = () => {
-  window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 };
 let $swiper = $(".swiper-container"),
-  $bottomSlide = null,
-  $bottomSlideContent = null,
-  mySwiper = new Swiper(".swiper-container", {
-    spaceBetween: 1,
-    slidesPerView: 2,
-    centeredSlides: !0,
-    roundLengths: !0,
-    breakpoints: { 100: { slidesPerView: 1 }, 1200: { slidesPerView: 2 } },
-    loop: !0,
-    loopAdditionalSlides: 3,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  }),
-  swiper = new Swiper(".libary_images", {
-    spaceBetween: 20,
-    slidesPerView: 4,
-    roundLengths: !0,
-    loop: !0,
-    loopAdditionalSlides: 30,
-    breakpoints: {
-      100: { slidesPerView: 1 },
-      600: { slidesPerView: 2 },
-      1200: { slidesPerView: 4 },
-    },
-    autoplay: { delay: 3e3 },
-    loop: !0,
-  });
+    $bottomSlide = null,
+    $bottomSlideContent = null,
+    mySwiper = new Swiper(".swiper-container", {
+        spaceBetween: 1,
+        slidesPerView: 2,
+        centeredSlides: !0,
+        speed: 800,
+        roundLengths: !0,
+        breakpoints: { 100: { slidesPerView: 1 }, 1200: { slidesPerView: 2 } },
+        loop: !0,
+        autoplay: { delay: 3000 },
+        loopAdditionalSlides: 30,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    }),
+    swiper = new Swiper(".libary_images", {
+        spaceBetween: 20,
+        slidesPerView: 4,
+        roundLengths: !0,
+        loop: !0,
+        loopAdditionalSlides: 30,
+        breakpoints: {
+            100: { slidesPerView: 1 },
+            600: { slidesPerView: 2 },
+            1200: { slidesPerView: 4 },
+        },
+        autoplay: { delay: 3000 },
+        loop: !0,
+    });
